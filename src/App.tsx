8bb382fb0bdf9594e98c47ee5c7807bd1dab8fd7
spaceDandy23 <carlospolarislubrica@gmail.com>
2025-04-07 19:39:54 +0800

    portlaris layout made

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="section">
+
+      </div>
+      <div className="section">
+        <div className="introduction">
+        introduction
+        </div>
+        <div className="about">
+        about
+        </div>
+        <div className="work-experience">
+        work-experience
+        </div>
+        <div className="education">
+        education
+        </div>        
+      </div>
+      <div className="section">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
